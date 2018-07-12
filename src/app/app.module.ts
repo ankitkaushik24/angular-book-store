3cb984c93ae903e129c12a632d5a5ed6f04255fd
mehul-jain <mehul@infrrd.ai>
2018-07-12 12:08:15 +0530

    added login form template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UserModule } from './user/user.module';
 import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
 import {routes} from './app-routes/app.routes';
@@ -11,6 +11,7 @@ import {routes} from './app-routes/app.routes';
   ],
   imports: [
     BrowserModule,
+    UserModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
