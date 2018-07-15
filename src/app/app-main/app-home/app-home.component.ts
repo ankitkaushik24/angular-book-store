dc699477ae12cdd8cbbe1152da6027da4d5b4561
swatikharpor <skharpor17@gmail.com>
2018-07-15 17:25:06 +0530

    Home Page  and changes the nav bar color

--- src/app/app-main/app-home/app-home.component.ts
+++ src/app/app-main/app-home/app-home.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-home.component.scss']
 })
 export class AppHomeComponent implements OnInit {
-
+  categories =[{
+    'name': 'Drama',
+  },{
+    'name': 'Science Fiction',
+  },{
+    'name': 'Biography',
+  },{
+    'name': 'Lifestyle',
+  },{
+    'name': 'Mystery',
+  },{
+    'name': 'Poetry',
+  },{
+    'name': 'Romance',
+  },{
+    'name': 'Comics',
+  },{
+    'name': 'Travel',
+  },{
+    'name': 'Adventure',
+  },{
+    'name': 'History',
+  },{
+    'name': 'Art',
+  }]
+newArray;
   constructor() { }
 
   ngOnInit() {
+   this.newArray = this.categories.reduce(function(categories,item,index) {
+
+      if(index % 3 === 2) {
+        categories.right.push(item);
+      }
+      else if(index % 3 === 1 ){
+        categories.middle.push(item);
+      }
+      else{
+        categories.left.push(item);
+      }
+      return categories;
+    },{left:[],middle:[],right:[]});
   }
+
 
 }
--- src/app/app-routes/app.routes.ts
+++ src/app/app-routes/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from '../app-authentication/login/login.component';
 import {WelcomeComponent} from '../welcome/welcome.component';
 import {NotificationComponent} from '../app-notification/app-notification.component'
+import { AppHomeComponent } from '../app-main/app-home/app-home.component';
 
 export const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'messages', component: NotificationComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
+  {path: 'notification', component: NotificationComponent},
+  {path: 'home', component: AppHomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
